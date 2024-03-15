b9430ee866dcfe5070158c71b53fd621c13f7ce3
Apsurt <tymon@becella.eu>
2024-03-15 19:29:24 +0100

    Move logger module

--- quasar/chess/__init__.py
+++ quasar/chess/__init__.py
@@ -5,5 +5,4 @@ from .pieces import Piece, PieceFactory, PieceColor, PieceName
 from .moves import Move
 from .point import Point
 from .utils import *
-from .logger import logger, clear_logs
 from .errors import *
