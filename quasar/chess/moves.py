3548eb490a57bd863518c0396219ecdee0b9fb8c
Apsurt <tymon@becella.eu>
2024-03-16 12:21:39 +0100

    Add color_to_move parameter to Move class constructor

--- quasar/chess/moves.py
+++ quasar/chess/moves.py
@@ -3,6 +3,7 @@ This module contains the Move and MoveFlags classes.
 """
 
 from dataclasses import dataclass
+from .pieces import PieceColor
 from .point import Point
 
 @dataclass
@@ -10,21 +11,27 @@ class Move:
     """
     The Move class is responsible for storing the moves in the game.
     """
-    def __init__(self, source: Point, target: Point) -> None:
+    def __init__(self, color_to_move: PieceColor, source: Point, target: Point) -> None:
         """
         The constructor for the Move class.
 
+        :param color_to_move: Color of the piece making the move.
+        :type color_to_move: PieceColor
         :param source: Point where the move starts.
         :type source: Point
         :param target: Point where the move ends.
         :type target: Point
         """
+        self.color_to_move = color_to_move
         self.source = source
         self.target = target
         self.moved = None
         self.captured = None
         self.legal = True
         self.flags = MoveFlags()
+
+    def __str__(self) -> str:
+        return f"{self.moved} {self.source} -> {self.target}"
 
 @dataclass
 class MoveFlags:
